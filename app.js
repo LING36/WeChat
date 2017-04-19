6fa4fd64add1867f8e3222720bda22b38ae5c6d6
LING36 <1wling123@sina.cn>
2017-04-19 19:08:14 +0800

    微信小程序

---
+++ app.js
@@ -0,0 +1,43 @@
+//app.js
+App({
+  //当程序初始化的时候执行onLaunch里面的内容
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+  //小程序显示的时候触发该事件
+  onShow: function () {
+    console.log("生命周期-监听小程序显示的时候触发")
+  },
+  //小程序隐藏的时候触发该事件
+  onHide: function () {
+    console.log("生命周期-监听小程序隐藏的时候触发")
+  },
+  //
+  //定义全局的方法
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  //定义的全局属性
+  globalData: {
+    userInfo: null,
+    pass: "123456"
+  }
+})
