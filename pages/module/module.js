80fe21208152f60e920b6a9bb4ace14f285a005c
LING36 <1wling123@sina.cn>
2017-04-19 18:59:40 +0800

    组件

---
+++ pages/module/module.js
@@ -0,0 +1,20 @@
+Page({
+  data: {
+    iconType: [
+      'success', 'info', 'warn', 'waiting', 'safe_success', 'safe_warn',
+      'success_circle', 'success_no_circle', 'waiting_circle', 'circle', 'download',
+      'info_circle', 'cancel', 'search', 'clear'
+    ]
+  },
+  EventHandle: function (e) {
+    console.log(e);
+    console.log("11111");
+  },
+  scrolltolower: function () {
+    console.log("22222");
+  },
+  tapChange: function () {
+
+  },
+
+})
